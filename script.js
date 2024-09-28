f32f57f95b9274295823b57a4ef76a15c5a323f1
Joji <bengazanfer2@gmail.com>
2024-09-28 23:22:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const skills = document.querySelectorAll('.skill-progress');
-    skills.forEach(skill => {
-        const width = skill.style.width;
-        skill.style.width = '0%';
-        setTimeout(() => {
-            skill.style.width = width;
-        }, 100);
-    });
-});
+
